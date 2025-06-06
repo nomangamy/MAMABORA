5c2c6df534c68c5121c5e050ded9763382a8a43a
nomanGamy <kiplimochege@gmail.com>
2025-06-06 05:50:20 +0300

    dealt with the splash screen frontend

---
+++ Frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './mamaBora.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="splash-screen">
+        <h1>Mama Bora Platform</h1>
+        <img src={logo} className="logo" alt="logo" />
+        <p>post-natal care made easy</p>
+        <p>Join us in helping you raise your child
+today.we value your post-natal journey.
+</p>
+    </div>
+  );
+}
+
+export default App;
